c1017dbd3253fb9b33cec78de3033088028bf7b2
iaksharhate <iaksharhate@gmail.com>
2023-11-28 13:44:10 +0530

    added : signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user-auth/login/login.component';
+import { SignupComponent } from './user-auth/signup/signup.component';
 
 const routes: Routes = [
-  { path:'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
